2ec748483d2aa29ace222e28876f421de11ff799
Thomas Jakobi <thomas.jakobi@partout.info>
2017-10-04 16:11:35 +0200

    Normalize code

--- core/components/ressavesort/elements/plugins/ressavesort.plugin.php
+++ core/components/ressavesort/elements/plugins/ressavesort.plugin.php
@@ -1,20 +1,22 @@
 <?php
 /**
- * ResSaveSort
+ * ResSaveSort Plugin
  *
  * @package ressavesort
- * @subpackage plugin
+ * @subpackage pluginfile
  *
  * @var modX $modx
  * @var array $scriptProperties
  */
+
+$className = 'ResSaveSort' . $modx->event->name;
+
 $corePath = $modx->getOption('ressavesort.core_path', null, $modx->getOption('core_path') . 'components/ressavesort/');
 /** @var ResSaveSort $ressavesort */
 $ressavesort = $modx->getService('ressavesort', 'ResSaveSort', $corePath . 'model/ressavesort/', array(
     'core_path' => $corePath
 ));
 
-$className = 'ResSaveSort' . $modx->event->name;
 $modx->loadClass('ResSaveSortPlugin', $ressavesort->getOption('modelPath') . 'ressavesort/events/', true, true);
 $modx->loadClass($className, $ressavesort->getOption('modelPath') . 'ressavesort/events/', true, true);
 if (class_exists($className)) {
