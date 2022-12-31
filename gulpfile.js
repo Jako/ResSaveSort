0d7d8273ae218405c8538da2d1f4074dd757ebed
Thomas Jakobi <thomas.jakobi@partout.info>
2022-12-31 01:32:25 +0100

    Normalize build scripts

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,7 @@ pkg.dependencies.forEach(function (dependency, index) {
     }
 });
 
-gulp.task('scripts-mgr', function () {
+const scriptsMgr = function () {
     return gulp.src([
         'source/js/mgr/ressavesort.js',
         'source/js/mgr/ressavesort.grid.js'
@@ -43,13 +43,13 @@ gulp.task('scripts-mgr', function () {
         .pipe(uglify())
         .pipe(header(banner + '\n', {pkg: pkg}))
         .pipe(gulp.dest('assets/components/ressavesort/js/mgr/'))
-});
+};
+gulp.task('scripts', gulp.series(scriptsMgr));
 
-gulp.task('sass-mgr', function () {
+const sassMgr = function () {
     return gulp.src([
         'source/sass/mgr/ressavesort.scss'
     ])
-        .pipe(concat('ressavesort.scss'))
         .pipe(sass().on('error', sass.logError))
         .pipe(postcss([
             autoprefixer()
@@ -69,46 +69,48 @@ gulp.task('sass-mgr', function () {
             suffix: '.min'
         }))
         .pipe(footer('\n' + banner, {pkg: pkg}))
-});
+        .pipe(gulp.dest('assets/components/ressavesort/css/mgr/'))
+};
+gulp.task('sass', gulp.series(sassMgr));
 
-gulp.task('bump-copyright', function () {
+const bumpCopyright = function () {
     return gulp.src([
         'core/components/ressavesort/model/ressavesort/ressavesort.class.php',
         'core/components/ressavesort/src/ResSaveSort.php'
     ], {base: './'})
         .pipe(replace(/Copyright 2013(-\d{4})? by/g, 'Copyright ' + (year > 2013 ? '2013-' : '') + year + ' by'))
         .pipe(gulp.dest('.'));
-});
-gulp.task('bump-version', function () {
+};
+const bumpVersion = function () {
     return gulp.src([
         'core/components/ressavesort/src/ResSaveSort.php'
     ], {base: './'})
         .pipe(replace(/version = '\d+\.\d+\.\d+[-a-z0-9]*'/ig, 'version = \'' + pkg.version + '\''))
         .pipe(gulp.dest('.'));
-});
-gulp.task('bump-docs', function () {
+};
+const bumpDocs = function () {
     return gulp.src([
         'mkdocs.yml',
     ], {base: './'})
         .pipe(replace(/&copy; 2013(-\d{4})?/g, '&copy; ' + (year > 2013 ? '2013-' : '') + year))
         .pipe(gulp.dest('.'));
-});
-gulp.task('bump-requirements', function () {
+};
+const bumpRequirements = function () {
     return gulp.src([
         'docs/index.md',
     ], {base: './'})
         .pipe(replace(/[*-] MODX Revolution \d.\d.*/g, '* MODX Revolution ' + modxversion + '+'))
         .pipe(replace(/[*-] PHP (v)?\d.\d.*/g, '* PHP ' + phpversion + '+'))
         .pipe(gulp.dest('.'));
-});
-gulp.task('bump', gulp.series('bump-copyright', 'bump-version', 'bump-docs', 'bump-requirements'));
+};
+gulp.task('bump', gulp.series(bumpCopyright, bumpVersion, bumpDocs, bumpRequirements));
 
 gulp.task('watch', function () {
     // Watch .js files
-    gulp.watch(['source/js/**/*.js'], gulp.series('scripts-mgr'));
+    gulp.watch(['./source/js/**/*.js'], gulp.series('scripts'));
     // Watch .scss files
-    gulp.watch(['source/sass/**/*.scss'], gulp.series('sass-mgr'));
+    gulp.watch(['./source/sass/**/*.scss'], gulp.series('sass'));
 });
 
 // Default Task
-gulp.task('default', gulp.series('bump', 'scripts-mgr', 'sass-mgr'));
+gulp.task('default', gulp.series('bump', 'scripts', 'sass'));
