bf381e58edc40ed588c97189e7b5b2e22c32d98f
Thomas Jakobi <thomas.jakobi@partout.info>
2024-12-04 18:57:55 +0100

    Fix lexicon typos

--- core/components/ressavesort/lexicon/en/setting.inc.php
+++ core/components/ressavesort/lexicon/en/setting.inc.php
@@ -8,4 +8,4 @@
 $_lang['setting_ressavesort.debug'] = 'Debug';
 $_lang['setting_ressavesort.debug_desc'] = 'Log debug information in the MODX error log.';
 $_lang['setting_ressavesort.sorts'] = 'Sort Configurations';
-$_lang['setting_ressavesort.sorts_desc'] = 'JSON encoded array of sort configurations. Example: <em>[{"sortby":"pagetitle","sortdir":"asc","sortcontainer":""}]</em>';
+$_lang['setting_ressavesort.sorts_desc'] = 'JSON-encoded array of sort configurations. Example: <em>[{"sortby":"pagetitle","sortdir":"asc","sortcontainer":""}]</em>';
